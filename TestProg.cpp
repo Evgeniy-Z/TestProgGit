3d30e4967103c44f63e3252d72f03ad6c15294e3
KarinaZaikina <karinchik20000@gmail.com>
2021-11-03 09:50:11 +0200

    First commit. First task, print data of product: name, price, weight. Second task, print data of purchase: count, total price, total weight.

---
+++ TestProg.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<cstring>
+#include<iomanip>
+using namespace std;
+ 
+class Product
+{
+private:
+    char name[80];
+    int price;
+    float weight;
+public:
+     Product()
+     {
+         strcpy(name, '');
+         price = 0;
+         weight = 0;
+     }
+     Product(char*str, int pr, float wgh)
+     {
+         strcpy(name, str);
+         price = pr;
+         weight = wgh;
+     }
+     int getPrice()  const
+     {
+         return price;
+     }
+     float getWeight()  const
+     {
+         return weight;
+     }
+};
+ 
+class Buy:public Product
+{
+private:
+    int col;
+    int total_price;
+    float total_weight;
+public:
+     Buy():Product()
+     {
+         col = 0;
+         total_price = 0;
+         total_weight = 0;
+     }
+     Buy(int c)
+    {
+       col = c;
+       int temp1 = getPrice();
+       float temp2 = getWeight();
+       total_price = temp1*c;
+       total_weight = temp2*c;
+    }
+     Buy(char*n, int p, float w, int c):  Product(n, p, w)
+     {
+         col = c;
+       total_price = c* getPrice();;
+       total_weight = c* getWeight();
+     }
+ 
+};
+ 
+class Check:private Buy
+{
+  public:
+      Check(): Buy()
+      {
+      }
+      Check(char*n, int p, float w, int cl):  Buy(n, p, w, cl)
+      {
+      }
+};
+ 
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    Check s1("Вареники", 235, 3.5, 5);
+ 
+    return 0;
+}
